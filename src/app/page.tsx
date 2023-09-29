6075571d2d9984c9be6a0e7feef9a53b7bd64174
Luiz-Hen-Reis <lhcontato2020@gmail.com>
2023-09-29 20:21:18 -0300

    refactor: adding PaymentMethod to Home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import ConversorInputContainer from "@/components/conversor-input-container";
+import PaymentMethod from "@/components/payment-method";
 
 export default function Home() {
   return (
     <>
       <main>
         <ConversorInputContainer />
+        <PaymentMethod />
       </main>
     </>
   );
