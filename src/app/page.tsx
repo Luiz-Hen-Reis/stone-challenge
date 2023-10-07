a3b561cf4a174cc3090f8b0cfc77f4755dd4df66
Luiz-Hen-Reis <lhcontato2020@gmail.com>
2023-10-07 14:23:58 -0300

    feature: providing the result logic and adding Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,39 @@
 "use client";
 
+import Button from "@/components/button";
 import ConversorInputContainer from "@/components/conversor-input-container";
+import ArrowLeft from "@/components/icons/arrow-left";
 import EnchangeIcon from "@/components/icons/exchange-icon";
 import PaymentMethod from "@/components/payment-method";
+import Result from "@/components/result";
 import { useAppContext } from "@/useAppContext";
-import styled from "styled-components";
-
-const Button = styled.button<{ disabled: boolean }>`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 1.6rem;
-  width: 14.9rem;
-  height: 5.6rem;
-  padding: 1.6rem;
-  border-radius: 0.8rem;
-  border: 1px;
-  cursor: pointer;
-  background-color: ${(props) =>
-    props.disabled ? "var(--medium-gray)" : "var(--brand-color-2)"};
-  font-family: inherit;
-  font-size: 1.6rem;
-  font-weight: 500;
-  color: #ffffff;
-`;
 
 export default function Home() {
-  const { taxValue, dolarValue } = useAppContext();
+  const { taxValue, dolarValue, result } = useAppContext();
 
   return (
     <>
       <main>
-        <ConversorInputContainer />
-        <PaymentMethod />
-        <Button disabled={taxValue === "" || dolarValue === "" ? true : false}>
-          <EnchangeIcon /> Converter
-        </Button>
+        {result === 0 && (
+          <>
+            <ConversorInputContainer />
+            <PaymentMethod />
+            <Button
+              disabled={taxValue === "" || dolarValue === "" ? true : false}
+            >
+              <EnchangeIcon /> Converter
+            </Button>
+          </>
+        )}
+        {result !== 0 && (
+          <>
+            <Button variant>
+              <ArrowLeft />
+              Voltar
+            </Button>
+            <Result value={result} />
+          </>
+        )}
       </main>
     </>
   );
