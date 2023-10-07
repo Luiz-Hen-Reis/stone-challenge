46bdf37159834144626f2e857cb25fe8e29585f9
Luiz-Hen-Reis <lhcontato2020@gmail.com>
2023-10-07 14:20:27 -0300

    feature: formatDolarValueToNumber

---
+++ src/utils/formatDolarValueToNumber.ts
@@ -0,0 +1,12 @@
+export const formatDolarValueToNumber = (dolarValue: string) => {
+  const dolarValueInNumberFormat = dolarValue.replace(",", ".");
+  const arrayOfStrings = dolarValueInNumberFormat.split("");
+  const justNumbersInStringFormat = arrayOfStrings.filter(
+    (value) => value !== "."
+  );
+  const joinedStrings = justNumbersInStringFormat.join("");
+  const joinedStringsInNumberFormat = joinedStrings.replace(",", ".");
+  const dolarValueInNumber = Number(joinedStringsInNumberFormat);
+
+  return dolarValueInNumber;
+};
